18de801c3f0b69e02aa7deff6a529085926889b5
Vasiliy_Kozubov` <kozubov27@gmail.com`>
2023-03-26 22:27:39 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+// let a = 10;
+
+// alert(a);
+
+// a = 20;
+
+// alert(a);
+
+// const createIphone = (2007);
+
+// alert (createIphone);
+
+// const name = (`Брендан Айком`);
+
+// alert (name);
+
+// let ten = 10;
+
+// let two = 2;
+
+// alert(ten + two);
+
+// alert(ten - two);
+
+// alert(ten * two);
+
+// alert(ten / two);
+
+// let A = Number(prompt(`Какое-то значение №1`));
+
+// let b = Number(prompt(`Какое-то значение №2`));
+
+// let result = A + b;
+
+// let degree = 2 ** 5;
+
+// alert(degree);
+
+// let nine = 9;
+
+// let two = 2;
+
+// let result = `${nine % two}`;
+
+// alert(result);
+
+// let a = `2`;
+
+// let b = `3`;
+
+// a = Number(a);
+
+// b = Number(b);
+
+// alert(a + b);
+
+// let a = 2;
+
+// let b = 3;
+
+// alert(a + b);
+
+// let a = 5;
+
+// let b = 6;
+
+// alert(`${a + b}px`);
+
+// let age = Number (prompt(`Сколько вам лет?`));
+
+// alert(age + 1);
+
+// let num = 1;
+
+// num -= 5;
+
+// num *= 5;
+
+// num /= 5;
+
+// num ++;
+
+// num --;
+
+// alert(num);
+
+// let name = prompt(`Ваше имя`);
+
+// alert(`Привет ${name}!`);
